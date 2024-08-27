5ad4201dd02c5a2d00920fd168d938ddd406cb64
joanna <jzajacszymura@gmail.com>
2024-08-28 00:30:26 +0200

    js learning

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ console.log('wiadomosc z konsoli');
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam obecnie ${age} lat`);
+
+
+const friend_name = 'Przemek';
+const phone_number = 1051050505;
+console. log(`Witaj przyjacielu! Miło Cię poznać ${friend_name}, czy Twój numer tel to: ${phone_number}?`);
